c4e1ca3741d60e6e5b7afb45c9b2a1f96d0659cd
bayusuryowibowo <bayusuryowibowo@gmail.com>
2023-08-02 00:42:29 +0700

    feat: add route calculation & map rendering
    1. npm config set @here:registry https://repo.platform.here.com/artifactory/api/npm/maps-api-for-javascript/
    2. install dependencies => @here/maps-api-for-javascript
    3. set VITE_HERE_MAPS_API_KEY on .env file
    4. create Map component & Map Page
    5. assign travelled distance to data state

---
+++ client/src/views/HomePage.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  
+}
+</script>
+
+<template>
+  <section class="min-h-screen">
+    <div class="max-w-7xl mx-auto py-24 px-4">
+      
+    </div>
+  </section>
+</template>
---
+++ client/src/views/MapPage.vue
@@ -0,0 +1,17 @@
+<script>
+import Map from '../components/Map.vue'
+
+export default {
+  components: {
+    Map
+  }
+}
+</script>
+
+<template>
+  <section class="min-h-screen">
+    <div class="max-w-7xl mx-auto py-24 px-4">
+      <Map />
+    </div>
+  </section>
+</template>
