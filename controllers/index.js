b11d608981bea39d2db7f26abcb4070f6d8a89ef
bayusuryowibowo <bayusuryowibowo@gmail.com>
2023-08-01 15:42:53 +0700

    feat: add POST /register
    1. user must input email, username, & password to register

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,21 @@
-
+const { User } = require("../models");
 
 class Controller {
-
+  static async register(req, res, next) {
+    try {
+      const { email, username, password } = req.body;
+      await User.create({
+        email,
+        username,
+        password,
+      });
+      res.status(201).json({
+        message: "Registration succeed!",
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
-module.exports = Controller
+module.exports = Controller;
