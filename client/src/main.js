170bd9d1be446036ad2c600427092320787089c4
bayusuryowibowo <bayusuryowibowo@gmail.com>
2023-08-02 14:28:20 +0700

    fix: client skeleton
    1. define markRaw on main.js file
    2. define useRentalStore on rentalStore.js file

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { markRaw } from 'vue'
 
 import App from './App.vue'
 import router from './router'
---
+++ client/src/stores/rentalStore.js
@@ -0,0 +1,11 @@
+import { defineStore } from 'pinia'
+import axios from 'axios'
+
+export const useRentalStore = defineStore('rental', {
+  state: () => {
+    return {}
+  },
+  actions: {
+    
+  }
+})
